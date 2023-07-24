f5a51263800e6bba6f6a0ce05278eb55a4549cd1
hg0710 <122251142+hg0710@users.noreply.github.com>
2023-07-25 00:25:32 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 1.95 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,8 @@
+import java.util.stream.LongStream;
+class Solution {
+    public long solution(int a, int b) {
+        
+        if(a < b) return LongStream.rangeClosed(a,b).sum();
+        else return LongStream.rangeClosed(b,a).sum();
+    }
+}
