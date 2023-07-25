c82c10c65734ca0f3290e36982c8723f6484b424
hg0710 <122251142+hg0710@users.noreply.github.com>
2023-07-26 00:31:06 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+       
+        StringBuilder str = new StringBuilder();
+        
+        for(int i =0; i<n ;i++){
+            
+            if(i % 2 ==0) str.append("수");
+            else str.append("박");
+        }
+        
+        return str.toString();
+    }
+}
