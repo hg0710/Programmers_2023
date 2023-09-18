2cdf643b900be82a81fdd40f84980f47dc0f8a71
hg0710 <122251142+hg0710@users.noreply.github.com>
2023-09-19 01:56:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.57 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        Map<String, Integer> map = new HashMap<>();
+
+        
+        for (int i =0; i<participant.length ;i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+
+        for (int i =0; i<completion.length ;i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+
+        
+        for (int i =0; i<participant.length ;i++) {
+            if (map.get(participant[i]) == 1) {
+                return participant[i];
+            }
+        }
+
+        return null;
+    }
+}
